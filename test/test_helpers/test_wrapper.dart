be3f6f4cd238e3f8e7858ae81fc4ab5f5dd16236
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 03:44:25 +0200

    Added UserSelectionViewModel provider to test wrapper

--- test/test_helpers/test_wrapper.dart
+++ test/test_helpers/test_wrapper.dart
@@ -1,6 +1,7 @@
 
 import 'package:flight_demo_v2/viewmodels/airport_list_view_model.dart';
 import 'package:flight_demo_v2/viewmodels/flight_list_view_model.dart';
+import 'package:flight_demo_v2/viewmodels/user_selection_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,6 +18,9 @@ class TestWrapper extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => AirportListViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserSelectionViewModel(),
         ),
       ],
       child: MaterialApp(
