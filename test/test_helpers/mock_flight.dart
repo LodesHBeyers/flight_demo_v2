880c631558801617124c84f1109e6b99bda2842a
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 03:56:56 +0200

    Added mock flight and erroneous flight

---
+++ test/test_helpers/mock_flight.dart
@@ -0,0 +1,32 @@
+
+import 'package:flight_demo_v2/models/flight_model.dart';
+
+final Flight mockFlight = Flight(
+  flightDate: DateTime.now(),
+  flightStatus: '',
+  departureAirport: '',
+  departureTimeZone: '',
+  departureDateTime:DateTime.now(),
+  departureTerminal: '',
+  departureGate: '',
+  arrivalAirport: '',
+  arrivalTimeZone: '',
+  arrivalDateTime:DateTime.now(),
+  airlineName: '',
+  flightNumber: '',
+);
+
+final Flight erroneousMockFlight = Flight(
+  flightDate: null,
+  flightStatus: '',
+  departureAirport: '',
+  departureTimeZone: '',
+  departureDateTime: null,
+  departureTerminal: '',
+  departureGate: '',
+  arrivalAirport: '',
+  arrivalTimeZone: '',
+  arrivalDateTime: null,
+  airlineName: '',
+  flightNumber: '',
+);
