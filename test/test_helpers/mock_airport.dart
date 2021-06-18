84c6e9b11386c8ddb0150c9f5f95e7726888a7d5
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 03:43:56 +0200

    Added mock airport

---
+++ test/test_helpers/mock_airport.dart
@@ -0,0 +1,8 @@
+import 'package:flight_demo_v2/models/airport_model.dart';
+
+final Airport mockAirport = Airport(
+  airportName: 'airportName',
+  iataCode: 'iataCode',
+  latitude: '123456789',
+  longitude: '123456789',
+);
