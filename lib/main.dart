e15e312abb530774c122d6ad5c60cfeb517b6578
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 06:21:51 +0200

    Included providers and initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
-import 'package:flight_demo_v2/temp.dart';
+import 'package:flight_demo_v2/viewmodels/airport_list_view_model.dart';
+import 'package:flight_demo_v2/viewmodels/flight_list_view_model.dart';
+import 'package:flight_demo_v2/viewmodels/user_selection_view_model.dart';
+import 'package:flight_demo_v2/views/airport_select_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+Client client = Client();
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flight Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => FlightListViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => AirportListViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserSelectionViewModel(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flight Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Material(child: AirportSelectScreen(client: client,)),
       ),
-      home: Material(child: Temp()),
     );
   }
 }
