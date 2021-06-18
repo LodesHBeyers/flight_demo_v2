95f8b1aef956144d2b30829c05cb7255701fab5c
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 02:15:54 +0200

    Webservice TDD implementation

--- lib/services/web_services.dart
+++ lib/services/web_services.dart
@@ -1,13 +1,78 @@
 import 'dart:convert';
+import 'package:flight_demo_v2/models/airport_model.dart';
 import 'package:flight_demo_v2/models/flight_model.dart';
 import 'package:http/http.dart' as http;
 
-String airportsAPItUrl =
+String airportsAPIUrl =
     'http://api.aviationstack.com/v1/airports?access_key=6867ad02c34e331babc046a0124b156a';
 
-String flightsAPItUrl =
-    'http://api.aviationstack.com/v1/flights?access_key=6867ad02c34e331babc046a0124b156a';
+String flightsAPIUrl =
+    'http://api.aviationstack.com/v1/flights?access_key=6867ad02c34e331babc046a0124b156a&limit=10';
 
-class WebService {
+class WebServices{
 
+  static Future<List<Flight>> fetchFlights({required http.Client client, String iataCode = ''}) async{
+    List<Flight> flightsList = [];
+    String url = flightsAPIUrl + (iataCode.isNotEmpty ? '&dep_iata=$iataCode' : '');
+
+    final http.Response response = await client.get(Uri.parse(url));
+
+    try{
+      if(response.statusCode == 200){
+        final jsonData = jsonDecode(response.body);
+
+        for(var data in jsonData['data']){
+          flightsList.add(Flight.fromJson(data));
+        }
+        return flightsList;
+      }else{
+       throw Exception('Unable to fetch flights');
+      }
+    }catch(e){
+      throw Exception('Unable to connect to the api');
+    }
+  }
+
+  static Future<List<Flight>> fetchMoreFlights({required http.Client client, String iataCode = ''}) async{
+    List<Flight> flightsList = [];
+    String url = flightsAPIUrl + (iataCode.isNotEmpty ? '&dep_iata=$iataCode' : '');
+
+    final http.Response response = await client.get(Uri.parse(url));
+
+    try{
+      if(response.statusCode == 200){
+        final jsonData = jsonDecode(response.body);
+
+        for(var data in jsonData['data']){
+          flightsList.add(Flight.fromJson(data));
+        }
+        return flightsList;
+      }else{
+        throw Exception('Unable to fetch flights');
+      }
+    }catch(e){
+      throw Exception('Unable to connect to the api');
+    }
+  }
+
+  static Future<List<Airport>> fetchAirports({required http.Client client}) async{
+    List<Airport> airportList = [];
+
+    final http.Response response = await client.get(Uri.parse(airportsAPIUrl));
+
+    try{
+      if(response.statusCode == 200){
+        final jsonData = jsonDecode(response.body);
+
+        for(var data in jsonData['data']){
+          airportList.add(Airport.fromJson(data));
+        }
+        return airportList;
+      }else{
+        throw Exception('Unable to fetch flights');
+      }
+    }catch(e){
+      throw Exception('Unable to connect to the api');
+    }
+  }
 }
---
+++ test/services/webservices.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+import 'package:flight_demo_v2/models/airport_model.dart';
+import 'package:flight_demo_v2/models/flight_model.dart';
+import 'package:flight_demo_v2/services/web_services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import '../test_helpers/mock_airports_data.dart';
+import '../test_helpers/mock_flights_data.dart';
+import 'webservices.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  late MockClient mockClient;
+
+  setUp(() {
+    mockClient = MockClient();
+  });
+
+  group(
+    'Flights api interaction',
+    () {
+      test(
+        'Given a call to fetchFlights When error code is returned Should throw an exception',
+        () async {
+          when(mockClient.get(any))
+              .thenAnswer((realInvocation) async => http.Response('', 404));
+
+          final result = WebServices.fetchFlights(client: mockClient);
+
+          expect(result, throwsException);
+        },
+      );
+
+      test(
+        'Given a call to fetchFlights When no error code is returned Should return a list of flights',
+        () async {
+          when(mockClient.get(any)).thenAnswer((realInvocation) async =>
+              http.Response(jsonEncode(mockFlightsData), 200));
+
+          final result = await WebServices.fetchFlights(client: mockClient);
+
+          expect(result, isA<List<Flight>>());
+        },
+      );
+
+      test(
+        'Given a call to fetchMoreFlights When error code is returned Should throw an exception',
+            () async {
+          when(mockClient.get(any))
+              .thenAnswer((realInvocation) async => http.Response('', 404));
+
+          final result = WebServices.fetchMoreFlights(client: mockClient);
+
+          expect(result, throwsException);
+        },
+      );
+
+      test(
+        'Given a call to fetchMoreFlights When no error code is returned Should return a list of flights',
+            () async {
+          when(mockClient.get(any)).thenAnswer((realInvocation) async =>
+              http.Response(jsonEncode(mockFlightsData), 200));
+
+          final result = await WebServices.fetchMoreFlights(client: mockClient);
+
+          expect(result, isA<List<Flight>>());
+        },
+      );
+    },
+  );
+
+  group(
+    'Airports api interaction',
+      (){
+        test(
+          'Given a call to fetchAirports When error code is returned Should throw an exception',
+              () async {
+            when(mockClient.get(any))
+                .thenAnswer((realInvocation) async => http.Response('', 404));
+
+            final result = WebServices.fetchAirports(client: mockClient);
+
+            expect(result, throwsException);
+          },
+        );
+
+        test(
+          'Given a call to fetchAirports When no error code is returned Should return a list of airports',
+              () async {
+            when(mockClient.get(any)).thenAnswer((realInvocation) async =>
+                http.Response(jsonEncode(mockAirportData), 200));
+
+            final result = await WebServices.fetchAirports(client: mockClient);
+
+            expect(result, isA<List<Airport>>());
+          },
+        );
+      },
+  );
+}
