41358556e130bf9d0db0e25a5e25ccc41149dd40
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-18 03:17:00 +0200

    Added widget wrapper for providers and material root

---
+++ test/test_helpers/test_wrapper.dart
@@ -0,0 +1,27 @@
+
+import 'package:flight_demo_v2/viewmodels/airport_list_view_model.dart';
+import 'package:flight_demo_v2/viewmodels/flight_list_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class TestWrapper extends StatelessWidget {
+  final Widget child;
+  const TestWrapper({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => FlightListViewModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => AirportListViewModel(),
+        ),
+      ],
+      child: MaterialApp(
+        home: child,
+      ),
+    );
+  }
+}
