ddaafb284246555bfc2c190a0fb71ab39d26e7fb
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-20 15:24:17 +0200

    FlightDetailView boilerplate

---
+++ lib/views/flight_detail_view.dart
@@ -0,0 +1,13 @@
+import 'package:flight_demo_v2/viewmodels/flights_view_model.dart';
+import 'package:flutter/material.dart';
+
+class FlightDetailScreen extends StatelessWidget {
+  final FlightViewModel flight;
+
+  const FlightDetailScreen({Key? key, required this.flight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container( );
+  }
+}
