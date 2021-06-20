81417b7ca6e3722785349c5dba3015fc10ae0a2c
Lodes Beyers <lodesbeyers1@gmail.com>
2021-06-20 20:20:52 +0200

    Implemented connection checker

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:connectivity/connectivity.dart';
 import 'package:flight_demo_v2/viewmodels/airport_list_view_model.dart';
 import 'package:flight_demo_v2/viewmodels/flight_list_view_model.dart';
 import 'package:flight_demo_v2/viewmodels/user_selection_view_model.dart';
@@ -5,6 +7,8 @@ import 'package:flight_demo_v2/views/airport_select_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:provider/provider.dart';
+
+import 'components/connection_checker_overlay.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,8 +36,55 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Material(child: AirportSelectScreen(client: client,)),
+        home: ConnectionWrapper()
       ),
     );
   }
 }
+
+class ConnectionWrapper extends StatefulWidget {
+  const ConnectionWrapper({Key? key}) : super(key: key);
+
+  @override
+  _ConnectionWrapperState createState() => _ConnectionWrapperState();
+}
+
+class _ConnectionWrapperState extends State<ConnectionWrapper> {
+  late StreamSubscription<ConnectivityResult> subscription;
+  bool hasConnection = true;
+
+  @override
+  initState() {
+    super.initState();
+    subscription = Connectivity().onConnectivityChanged.listen((
+        ConnectivityResult result) {
+      if(result == ConnectivityResult.none){
+        hasConnection = false;
+      }else{
+        hasConnection = true;
+      }
+      setState(() {
+
+      });
+    });
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    subscription.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Material(child: AirportSelectScreen(client: client,)),
+          ConnectionCheckerWrapper(hasConnection: hasConnection,)
+        ],
+      ),
+    );
+  }
+}
+
